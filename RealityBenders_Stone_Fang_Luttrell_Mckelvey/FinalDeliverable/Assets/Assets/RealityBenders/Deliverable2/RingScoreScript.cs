90d78603fcd89908cab513ec341d70527ce73e1f
wluttre <wluttre@g.clemson.edu>
2018-10-08 20:53:39 -0400

    commit of more assets

---
+++ RealityBenders_Stone_Fang_Luttrell_Mckelvey/FinalDeliverable/Assets/Assets/RealityBenders/Deliverable2/RingScoreScript.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RingScoreScript : MonoBehaviour {
+    public int remainingRings;
+    public AudioSource ding;
+    //public GUIText scoreText;
+	// Use this for initialization
+	void Start () {
+        remainingRings = 25;
+        UpdateScore();
+        ding = GetComponent<AudioSource>();
+	}
+
+    void OnEnable() {
+        RaceRing.OnScore += AddScore;
+    }
+
+    void OnDisable() {
+        RaceRing.OnScore -= AddScore;
+    }
+    void AddScore(RaceRing ring) {
+        if (remainingRings > 0) {
+            remainingRings--;
+            UpdateScore();
+        }
+
+        //see how this is done in deliverable 1
+        ding.Play();
+    }
+    void UpdateScore() {
+        //scoreText.text = "Remaining Rings: " + remainingRings;
+    }
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
